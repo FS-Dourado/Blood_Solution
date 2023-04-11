5ec76d1ba733d8a5329061a8ab629fb2d248bf74
Dourado <felipedourado920@gmail.com>
2023-04-11 20:54:15 -0300

    inserts finalizados

--- bd_bloodSolutions.sql
+++ bd_bloodSolutions.sql
@@ -1,12 +1,11 @@
 CREATE DATABASE blood_solutions;
 USE blood_solutions;
 
+-- Criação das tabelas
 CREATE TABLE empresa(
 	idEmpresa INT PRIMARY KEY auto_increment,
     nome VARCHAR(45),
-    cnpj VARCHAR(45),
-	email VARCHAR(45),
-    senha VARCHAR(45)
+    cnpj CHAR(15)
 );
 
 CREATE TABLE usuario(
@@ -51,11 +50,73 @@ CREATE TABLE historico(
     constraint fkSensor FOREIGN KEY (fkSensor) references sensor(idSensor)
 );
 
+
 CREATE TABLE contato(
 	idContato INT PRIMARY KEY auto_increment,
     nome VARCHAR(45),
     email VARCHAR(45),
-    mensagem VARCHAR(45)
+    mensagem VARCHAR(100)
 );
 
+-- Inserimento de dados
 
+INSERT INTO empresa VALUES
+	(null, 'Hospital Israelita Albert Einstein', 02159784000157),
+    (null, 'Hospital Sirio Libanes', 54833209000100),
+    (null, 'Hospital Oswaldo Cruz', 85560317000120);
+    
+INSERT INTO usuario VALUES
+	(null, 'Felipe Dourado', 'felipe@gmail.com', 'dourado123', 'Administrador', 1),
+    (null, 'Nicolly Juliani', 'nicolly@gmail.com', 'juliani_45', 'Funcionário', 1),
+    (null, 'Pedro Donizete', 'pedro@gmail.com', 'pedro_lucky', 'Administrador', 2),
+    (null, 'Arthur Garcia', 'garcia@gmail.com', 'garciarthur', 'Funcionário', 2),
+    (null, 'Marcela Mendes', 'marcella@gmail.com', 'm4rc3ll4_', 'Administrador', 3),
+    (null, 'Mirella Ot', 'mirella@gmail.com', 'ot_mirella', 'Funcionário', 3);
+    
+INSERT INTO local_empresa VALUES
+	(null, '#','Av. Albert Eintein, 627/701', 1),
+	(null, '#','Rua Dona Adma Jafet, 115', 2),
+    (null, '#','Rua Treze de Maio, 1815', 3);
+    
+INSERT INTO lote VALUES
+	(null, 25, '23', 1),
+    (null, 50, '24', 1),
+    (null, 20, '20', 2),
+    (null, 55, '15', 2),
+    (null, 40, '10', 3),
+    (null, 35, '14', 3);
+    
+INSERT INTO sensor VALUES
+	(null, 1, 1),
+    (null, 0, 1),
+    (null, 0, 2),
+    (null, 1, 2),
+    (null, 1, 3),
+    (null, 0, 3);
+    
+INSERT INTO historico VALUES
+	(null, 10.3, default, 1),
+    (null, 10, default, 1),
+    (null, 5.5, default, 2),
+    (null, 6.3, default, 2),
+    (null, 4.7, default, 3),
+    (null, 5.3, default, 3),
+    (null, 10.3, default, 4),
+    (null, 7, default, 4),
+    (null, 5.4, default, 5),
+    (null, 5.4, default, 5),
+    (null, 6.6, default, 6),
+    (null, 6.0, default, 6);
+    
+INSERT INTO contato VALUES
+	(null, 'Marcos Silva', 'marcos@gmail.com', 'Gostaria de saber, como contratar o serviço de vocês?'),
+    (null, 'Gabriel Santana', 'santana@gmail.com', 'Qual o preço médio do sistema de vocês?');
+    
+-- SELECTS DE TODAS AS TABELAS
+SELECT * FROM empresa;
+SELECT * FROM usuario;
+SELECT * FROM local_empresa;
+SELECT * FROM lote;
+SELECT * FROM sensor;
+SELECT * FROM historico;
+SELECT * FROM contato;
