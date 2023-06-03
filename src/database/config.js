b28a1b49c8f4bddc58a8db46089622fac6a81ca9
Dourado <felipedourado920@gmail.com>
2023-06-03 15:46:09 -0300

    Branch criada

--- src/database/config.js
+++ src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "blood_solutions",
-    user: "aluno",
-    password: "sptech",
+    user: "root",
+    password: "dourado29",
 };
 
 function executar(instrucao) {
